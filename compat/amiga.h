cda9f3b5d96b7434500e34422d3d8ff603533ce4
Vidar Hokstad <vidar@hokstad.com>
2011-02-03 05:50:30 -0500

    (Untested) refactoring to incorporate some changes required for AmigaOS4

--- compat/amiga.h
+++ compat/amiga.h
@@ -5,9 +5,13 @@
 #include <proto/exec.h>
 #include <proto/dos.h>
 
+/* FIXME: Verify if unistd.h may work for AROS too, not just AOS4 */
+#ifdef __AROS__
 #include <defines/bsdsocket.h>
-#include <exec/types.h>
 #undef connect
+#endif
+#include <exec/types.h>
+#include <unistd.h>
 
 #define is_dir_sep(c) (((c) == '/') || (c) == ':')
 
@@ -50,27 +54,14 @@ const char * hstrerror(int err);
 int getpagesize();
 int poll(struct pollfd *ufds, unsigned int nfds, int timeout);
 
-/* Work around a problem with the AROS gcc port */
-void __stack_chk_fail();
-
-/* Workaround for definition of connect as macro in AROS
-   headers, which collide with it being used as a structure
-   member in git */
-static inline int connect(int sockfd, const struct sockaddr *addr,
-	    socklen_t addrlen)
-{
-  return __connect_WB(SocketBase,sockfd,(struct sockaddr *)addr,addrlen);
-}
-
 static inline int select(int nfds, fd_set *readfds, fd_set *writefds,
 	   fd_set *exceptfds, struct timeval *timeout)
 {
+#ifdef __AROS__
   return WaitSelect(nfds,readfds,writefds,exceptfds, timeout,0);
-}
-
-static inline char *inet_ntoa(struct in_addr in)
-{
-  return Inet_NtoA(*(unsigned long *)&in);
+#else
+  return waitselect(nfds,readfds,writefds,exceptfds, timeout,0);
+#endif
 }
 
 #define is_absolute_path(path) (strchr(path,':') != 0)
@@ -82,5 +73,29 @@ static inline int chk_abort()
 }
 
 #define CHKABORT 1
+
+
+/**** AROS specific pieces ****/
+
+#ifdef __AROS__
+static inline char *inet_ntoa(struct in_addr in)
+{
+  return Inet_NtoA(*(unsigned long *)&in);
+}
+
+/* Workaround for definition of connect as macro in AROS
+   headers, which collide with it being used as a structure
+   member in git */
+static inline int connect(int sockfd, const struct sockaddr *addr,
+	    socklen_t addrlen)
+{
+  return __connect_WB(SocketBase,sockfd,(struct sockaddr *)addr,addrlen);
+}
+
+/* Work around a problem with the AROS gcc port */
+void __stack_chk_fail();
+
+#endif
+
 
 #endif
